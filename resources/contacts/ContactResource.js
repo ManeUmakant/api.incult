6ea819a25029b2f38ca58d889131540c3f766a5e
Umakant B <umakant.b@2adpro.com>
2019-12-04 15:32:26 +0530

    implemented group api

--- resources/contacts/ContactResource.js
+++ resources/contacts/ContactResource.js
@@ -1,8 +1,9 @@
 const ContactRouter = require('express').Router();
 const Contact = require('./Contact');
+const TokenMiddleware = require('../../util/TokenMiddleware');
 
 let contact = new Contact();
-ContactRouter.post('/contactSync', contact.contactSync);
+ContactRouter.post('/contactSync', TokenMiddleware.checkToken ,contact.contactSync);
 
 module.exports = ContactRouter;
 
